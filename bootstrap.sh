7b961870261ac2b1a32acb2a7e91563272c9f0ec
Joshua Samuel <sauhsoj@amazon.com>
2023-08-01 16:51:03 +1000

    more tweaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -228,22 +228,8 @@ echo ========================
 echo installing rust
 echo ========================
 echo ${normal}
-# see https://github.com/rust-lang/rustup#other-installation-methods for options
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
-# add rust to the path before proceeding
-source ~/.cargo/env
 
-rustup component add rust-src
-
-if [ ! -d ~/src/rust-analyzer ]; then
-  git clone https://github.com/rust-analyzer/rust-analyzer --depth 1 ~/src/rust-analyzer/
-  echo running rust-analyzer installer
-  ( cd ~/src/rust-analyzer && cargo xtask install )
-else
-  echo rust-analyzer already exists
-fi
-
-awk '! /^ *(#|$)/' "cargorequirements.txt"  | xargs -r -- cargo install
+. ./rust-install.sh
 
 echo ${bold}
 echo ===================================
@@ -287,6 +273,13 @@ if [ ! -d ~/.fzf ]; then
 else
   cd ~/.fzf && git pull && ./install && cd -
 fi
+
+echo ${bold}
+echo ================================
+echo Installing Starship prompt
+echo ================================
+echo ${normal}
+curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 sudo chsh $USER -s /bin/zsh
 
--- local/lib/defaultenv.zsh
+++ local/lib/defaultenv.zsh
@@ -18,4 +18,6 @@ fi
 export PAGER="less"
 export LESS="-EFiMQR~ --tabs=4"
 export AWS_PAGER="less"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+if [ -x brew ]; then
+  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+fi
