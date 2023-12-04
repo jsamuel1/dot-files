4b778484b366f65327aed6cb50db996c9dfec76a
Joshua Samuel <sauhsoj@amazon.com>
2023-12-04 23:44:08 +0000

    fix neovim checks

--- install_neovim_to_amazonlinux.sh
+++ install_neovim_to_amazonlinux.sh
@@ -11,19 +11,19 @@ is_amazonlinux2 || is_amazonlinux2023 || exit
 sudo yum groups install -y Development\ tools
 
 if is_amazonlinux2; then
-    sudo yum install openssl11-devel wget -y
+	sudo yum install openssl11-devel wget -y
 
-    (
-        cd "$(mktemp -d)" || exit
-        wget https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz
-        tar -xvzf cmake-3.22.1.tar.gz
-        cd cmake-3.22.1 || exit
-        ./bootstrap
-        make
-        sudo make install || exit
-    )
+	(
+		cd "$(mktemp -d)" || exit
+		wget https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz
+		tar -xvzf cmake-3.22.1.tar.gz
+		cd cmake-3.22.1 || exit
+		./bootstrap
+		make
+		sudo make install || exit
+	)
 else
-    sudo yum install openssl-devel wget cmake python python-devel python-pip -y
+	sudo yum install openssl-devel wget cmake python python-devel python-pip -y
 fi
 
 pip3 install pynvim setuptools --upgrade
@@ -31,29 +31,29 @@ gem install neovim
 npm install -g neovim
 
 (
-    pushd . >/dev/null || exit
+	pushd . >/dev/null || exit
 
-    #Get or update neovim github repo
-    mkdir -p ~/src
-    cd ~/src || exit
-    if [ ! -e ~/src/neovim ]; then
-        git clone https://github.com/neovim/neovim
-    else
-        cd neovim || exit
-        git pull origin
+	#Get or update neovim github repo
+	mkdir -p ~/src
+	cd ~/src || exit
+	if [ ! -e ~/src/neovim ]; then
+		git clone https://github.com/neovim/neovim
+	else
+		cd neovim || exit
+		git pull origin stable
 
-        # don't build if installed nvim is same git hash
-        # shellcheck disable=SC2143
-        [[ -x nvim &&
-            $(nvim -v | grep -q "$(git rev-parse --short HEAD)") ]] &&
-            exit
-    fi
+		# don't build if installed nvim is same git hash
+		# shellcheck disable=SC2143
+		[[ $(which nvim) &&
+		$(nvim -v || grep -q "$(git rev-parse --short HEAD)") ]] &&
+			exit
+	fi
 
-    cd ~/src/neovim || exit
-    git checkout stable
-    make clean
-    make CMAKE_BUILD_TYPE=Release
-    sudo make install
+	cd ~/src/neovim || exit
+	git checkout stable
+	make clean
+	make CMAKE_BUILD_TYPE=Release
+	sudo make install
 
-    popd >/dev/null || exit
+	popd >/dev/null || exit
 )
