96fc94fcd862dccb446de44e4fff23b30687ba52
Joshua Samuel <sauhsoj@amazon.com>
2023-12-16 10:55:59 +0000

    add docker to dnf installs

--- dnf-install.sh
+++ dnf-install.sh
@@ -7,17 +7,18 @@ sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.re
 
 xargs -a <(awk '! /^ *(#|$)/' "dnfrequirements.txt") -r -- sudo dnf -y install
 
-if [[ $GUI -eq 1 ]]; then
-    echo ${bold}
-    echo =================
-    echo installing vscode
-    echo =================
-    echo ${normal}
-    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    if [ ! -f /etc/yum.repos.d/vscode.repo  ]; then
-        sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-        sudo dnf check-update
-    fi
-    sudo dnf -y install code
-fi
+sudo usermod -aG docker $USER
 
+if [[ $GUI -eq 1 ]]; then
+	echo ${bold}
+	echo =================
+	echo installing vscode
+	echo =================
+	echo ${normal}
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	if [ ! -f /etc/yum.repos.d/vscode.repo ]; then
+		sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+		sudo dnf check-update
+	fi
+	sudo dnf -y install code
+fi
