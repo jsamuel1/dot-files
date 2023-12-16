6fea830c80b0a0b8375ec0fa4db01da176a9ff1d
Joshua Samuel <sauhsoj@amazon.com>
2023-12-16 09:10:30 +0000

    Fix OMZ installer

--- install_oh_my_zsh.sh
+++ install_oh_my_zsh.sh
@@ -5,33 +5,33 @@ source helpers.sh
 
 heading "Setup zsh environment"
 
-if [ ! -d ~/.fzf ]; then
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install --bin --completion --no-key-bindings --no-update-rc --no-bash --no-fish
+if [ ! -d $HOME/.fzf ]; then
+	git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+	$HOME/.fzf/install --bin --completion --no-key-bindings --no-update-rc --no-bash --no-fish
 else
-  git -C ~/.fzf pull
-  ~/.fzf/install --bin --completion --no-key-bindings --no-update-rc --no-bash --no-fish
+	git -C $HOME/.fzf pull
+	$HOME/.fzf/install --bin --completion --no-key-bindings --no-update-rc --no-bash --no-fish
 fi
 
 #
-if [ ! -d ~/.oh-my-zsh ]; then
-  subheading "installing Oh-My-Zsh"
-  git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+	subheading "installing Oh-My-Zsh"
+	git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 elif [ -d "$ZSH/tools" ]; then
-  subheading "updating Oh-My-Zsh"
-  "$ZSH/tools/upgrade.sh" -vminimal
+	subheading "updating Oh-My-Zsh"
+	"$ZSH/tools/upgrade.sh" -vminimal
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}" ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 else
-  git -C "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}" pull
+	git -C "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}" pull
 fi
 
-if [ ! -d ~/.local/lib/zsh-completions ]; then
-  git clone https://github.com/zsh-users/zsh-completions.git ~/.local/lib/zsh-completions
+if [ ! -d $HOME/.local/lib/zsh-completions ]; then
+	git clone https://github.com/zsh-users/zsh-completions.git $HOME/.local/lib/zsh-completions
 else
-  git -C ~/.local/lib/zsh-completions pull
+	git -C $HOME/.local/lib/zsh-completions pull
 fi
 
 sudo chsh -s "$(which zsh)" "$USER" || true
