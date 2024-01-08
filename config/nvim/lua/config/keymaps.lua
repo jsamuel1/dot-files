8f7432c466a6b5d4c1241949e67eeb4c45112746
Joshua Samuel <sauhsoj@amazon.com>
2024-01-08 15:58:04 +1100

    tweak

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+if vim.g.vscode then
+  -- Toggle Comment
+  vim.keymap.set("x", "gc", "<Plug>VSCodeCommentary", {})
+  vim.keymap.set("n", "gc", "<Plug>VSCodeCommentary", {})
+  vim.keymap.set("o", "gc", "<Plug>VSCodeCommentary", {})
+  vim.keymap.set("n", "gcc", "<Plug>VSCodeCommentaryLine", {})
+end
