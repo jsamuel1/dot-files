3ea5e29aec0b8c87a76283d0ac6e7f6967afb979
Joshua Samuel <sauhsoj@amazon.com>
2026-05-25 16:16:48 +1000

    Refresh LazyVim extras and drop manual plugin overrides
    Add lazyvim.plugins.extras.ai.claudecode and switch from the standalone
    formatting.biome extra to lang.typescript.biome. Remove the manual
    treesitter ensure_installed list, vim-python-pep8-indent, and the
    amazonq plugin block from plugins.lua — LazyVim extras now cover the
    treesitter parsers, and AmazonQNVim is no longer in use.

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -35,36 +35,4 @@ return {
         },
     }
   },
-  -- Treesitter-integration
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "comment", -- for tags like TODO:, FIXME(user)
-        "dockerfile",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-      })
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      opts.sync_install = false
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      opts.auto_install = true
-    end,
-  },
-
-  -- Smart indentation for Python
-  { "Vimjas/vim-python-pep8-indent", ft = { "python" } },
-
-  {
-    name = "amazonq",
-    url = 'https://github.com/awslabs/amazonq.nvim.git',
-    -- url = "ssh://git.amazon.com/pkg/AmazonQNVim",
-    -- dir = "~/src/AmazonQNVim/src/aws/AmazonQNVim/",
-    opts = {
-      ssoStartUrl = "https://amzn.awsapps.com/start",
-      -- debug = true,
-    },
-  },
 }
